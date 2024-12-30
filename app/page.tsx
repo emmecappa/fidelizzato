3d15bb6952265fc90ef5efd44c28e45098d27f51
emmecappa <marco.casto96@gmail.com>
2024-12-31 00:58:10 +0100

    fix: css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
 <>
 
-<h1 className="font-bold text-[100px]">This is an unofficial Artifact_app</h1>
+<h1 className="font-bold lg:text-[100px] text-[60px]">This is an unofficial Artifact_app</h1>
 </>  
   );
 }
