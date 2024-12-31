d30e46b354a5273d28b2e4792dfd0f643d8fcd32
emmecappa <117775229+emmecappa@users.noreply.github.com>
2024-12-31 11:06:57 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   },
   darkMode: "class",
   plugins: [nextui()],
-  basePath: "/fidalizzato",
+  basePath: "/fidelizzato",
   output: "export",
   reactStrictMode: true,
 };
